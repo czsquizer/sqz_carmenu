50e1b3ea7b89ac24dfec2441bf4eca2774132782
Jiricech2059 <78664588+Jiri2059@users.noreply.github.com>
2022-09-07 20:05:35 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,3 +4,9 @@ Config.FrontCruiseSpeedControl = 11 -- Key to set front speed crusier on
 Config.CruiserControl = 10 -- Key to set Crusier On
 Config.OpenCarMenu = 344 -- Key to open Main (control menu)
 Config.minimalCrusierSpeed = 10 -- Minimal speed (in kmh) to turn Cruiser On
+Config.OnlyJob = false
+Config.Jobs = {
+  police,
+  mechanic,
+  sheriff
+}
