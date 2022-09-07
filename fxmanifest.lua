c97cab729908a1933ac815904cadebfb2f8bb1aa
Jiricech2059 <78664588+Jiri2059@users.noreply.github.com>
2022-09-07 20:00:41 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,16 +4,16 @@ game 'gta5'
 
 description 'SQZ Car Control'
 
-version '1.0.3'
+version '1.0.4'
+
+shared_scripts {
+    'config.lua',
+    '@es_extended/imports.lua',
+}
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'config.lua',
 	'client/main.lua',
 	'locales/cs.lua',
 	'locales/en.lua'
-}
-
-server_scripts {
-	'config.lua'
 }
